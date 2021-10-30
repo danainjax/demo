cc740c8063053a7631dd747fe30852a3a5c3372c
danainjax <danainjax@gmail.com>
2021-10-30 13:10:56 -0400

    added untracked

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/bullshitio'>Bull</Link>
+            </li>
+            <li>
+              <Link to='/ok'>Ok</Link>
+            </li>
+
+            <li>
+              <Link to='/gong'>Gong</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path='/ok'>
+            <Ok />
+          </Route>
+          <Route path='/Deal'>
+            <Deal />
+          </Route>
+          <Route path='/gong'>
+            <Gong />
+          </Route>
+          <Route path='/bullshitio'>
+            <Bull />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>
+}
+
+function Bull() {
+  return <h2> Bullshitio </h2>
+}
+
+function Ok() {
+  return <h2> Ok </h2>
+}
+
+function Deal() {
+  return <h2>Deal</h2>
+}
+function Gong() {
+  return <h2> Gong </h2>
+}
